23b3cad782c820fcacb7b9db33ed9a30ee290367
Lucas Brentano <brentano.l@gmail.com>
2021-08-17 20:53:30 -0300

    TDE02 - Ex03.java

--- Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex03.java
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex03.java
@@ -1,4 +1,5 @@
 package tde_02;
+import java.util.Scanner;
 
 /*Usando a classe JOptionPane para entrada de dados, elabore uma classe que receba a nota de duas provas e de um trabalho.
  Calcule e mostre a média e o resultado final (aprovado ou reprovado). Para calcular a média utilize a fórmula seguinte:
@@ -8,6 +9,29 @@ public class Ex03 {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        double testGrade1;
+        double testGrade2;
+        double workGrade;
+
+        System.out.println("Type the first grade: ");
+        testGrade1 = input.nextDouble();
+
+        System.out.println("Type the second grade: ");
+        testGrade2 = input.nextDouble();
+
+        System.out.println("Type the work grade: ");
+        workGrade = input.nextDouble();
+
+        double finalGrade = (testGrade1+testGrade2+workGrade)/3;
+
+        System.out.print("Your grade is "+finalGrade+" and your situation is: ");
+        if (finalGrade >= 6){
+            System.out.print("PASSED");
+        }else{
+            System.out.print("FAILED");
+        }
 
     }
 
