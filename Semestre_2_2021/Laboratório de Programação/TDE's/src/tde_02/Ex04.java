f5a68c54dd935932a6770dab12d1ee4890044cbe
Lucas Brentano <brentano.l@gmail.com>
2021-08-18 01:20:41 -0300

    Created ex04.java

---
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex04.java
@@ -0,0 +1,22 @@
+package tde_02;
+import java.util.Scanner;
+
+/*
+Considere a seguinte informação a respeito do cálculo da aposentadoria. Para se aposentar por idade, as mulheres precisam ter atingido 60
+anos de idade e os homens, 65. Já na aposentadoria por tempo de contribuição, o tempo mínimo exigido é de 30 anos para as mulheres e de 35
+ anos para os homens. Com base nessas informações, elabore uma classe que receba a idade de uma pessoa, seu sexo e a quantidade de anos de
+ contribuição. A seguir, calcule a quantidade de anos que falta de contribuição para se aposentar, ou então emita a mensagem: “você já tem o direito a aposentadoria”.
+*/
+
+public class Ex04 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name = "Pablo";
+
+        System.out.println(name);
+
+    }
+
+}
