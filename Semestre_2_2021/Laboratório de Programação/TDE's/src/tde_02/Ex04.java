5a5ef46e7fd6bea3b72a95b3276671821987ee1f
Lucas Brentano <brentano.l@gmail.com>
2021-08-23 22:40:53 -0300

    ex04

--- Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex04.java
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex04.java
@@ -13,9 +13,36 @@ public class Ex04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String name = "Pablo";
+        System.out.println("KNOW WHEN YOU CAN RETIRED");
+        System.out.println("Type your gender(m/f): ");
+        String gender = input.next();
 
-        System.out.println(name);
+        System.out.println("Type your age: ");
+        int age = input.nextInt();
+
+        System.out.println("Type the years of work contribution: ");
+        int contributionYears = input.nextInt();
+
+        boolean notRetired = true;
+
+        if(gender=="m"){
+            if(age > 65 || contributionYears > 30){
+                System.out.println("You can retire man.");
+                notRetired = false;
+            }
+        }else{
+            if(age > 60 || contributionYears > 30){
+                System.out.println("You can retire.");
+                notRetired = false;
+            }
+        }
+
+        if(notRetired){
+            System.out.println("You are not retired.");
+        }
+
+
+
 
     }
 
