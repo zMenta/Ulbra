a0245ddf928ef430fcaf0843725bf00af6aa0264
Lucas Brentano <brentano.l@gmail.com>
2021-08-31 21:55:51 -0300

    Ex06 update - TDE2

--- Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex06.java
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_02/Ex06.java
@@ -1,4 +1,5 @@
 package tde_02;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*Na área da eletrônica, em um circuito em série temos que a resistência equivalente (total) desse circuito é obtida mediante a soma de todas as
@@ -10,7 +11,45 @@ public class Ex06 {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
+        Scanner input;
+        ArrayList<Integer> resistances;
+
+        input = new Scanner(System.in);
+        resistances = new ArrayList<Integer>();
+
+        int equivalentResistance = 0;
+        int lowestResistance = 0;
+        int highestResistance = 0;
+        int typedResistance;
+
+        for(int i = 0; i < 4; i++){
+            System.out.println("Please type the "+(i+1)+"th resistance:");
+            typedResistance = input.nextInt();
+            resistances.add(typedResistance);
+
+            equivalentResistance += typedResistance;
+
+            if(i == 0){
+                lowestResistance = resistances.set(i,typedResistance);
+                highestResistance = resistances.set(i,typedResistance);
+            }else {
+                if (typedResistance < lowestResistance) {
+                    lowestResistance = typedResistance;
+                }
+
+                if (typedResistance > highestResistance){
+                    highestResistance = typedResistance;
+                }
+            }
+        }
+
+        equivalentResistance = equivalentResistance / resistances.size();
+
+        System.out.println(resistances);
+
+        System.out.println(resistances.get(2));
+        System.out.println(resistances.get(0));
 
 
 
