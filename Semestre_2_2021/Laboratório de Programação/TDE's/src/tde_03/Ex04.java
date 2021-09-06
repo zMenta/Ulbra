dc8da1b7efbf8e60c2db4dc93f9ca444e5b35ea0
Lucas Brentano <brentano.l@gmail.com>
2021-09-06 01:41:03 -0300

    [tde04] - Ex4

---
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_03/Ex04.java
@@ -0,0 +1,27 @@
+package tde_03;
+import java.util.Scanner;
+
+/*4 - Construa uma classe que receba uma frase qualquer e mostre essa frase de trás para a frente e sem espaços em branco,*/
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String phrase;
+        String newPhrase = "";
+
+        System.out.println("Type an phrase: ");
+        phrase = input.next();
+
+        phrase.replaceAll("\\s","");
+
+        for(int i = phrase.length()-1; i >= 0; i--){
+            newPhrase += phrase.charAt(i);
+        }
+
+        System.out.println(phrase.length());
+        System.out.println(phrase);
+        System.out.println(newPhrase);
+
+
+    }
+}
