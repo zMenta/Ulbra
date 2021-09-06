60f8ef21bd077d0563fa52bcdbc3522e206ead69
Lucas Brentano <brentano.l@gmail.com>
2021-09-06 15:23:57 -0300

    [tde03] Ex06

---
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_03/Ex06.java
@@ -0,0 +1,9 @@
+package tde_03;
+
+/*6 - Crie uma classe que exiba o substring de uma string FRASE composto por todos os caracteres de FRASE exceto o último caracter.*/
+
+public class Ex06 {
+    public static void main(String[] args) {
+
+    }
+}
