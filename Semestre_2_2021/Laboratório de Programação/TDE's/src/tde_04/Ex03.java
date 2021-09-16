feb0c781a0ec6f9da23a10791743ecaa49760797
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-09-15 23:56:11 -0300

    [tde04] Ex03

---
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/tde_04/Ex03.java
@@ -0,0 +1,17 @@
+package tde_04;
+
+/*Declare duas maneiras de declarar um array unidimensional de 12 doubles.*/
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        double[] array1;
+
+        double array2[];
+
+        double[] array = new double[12];
+
+        //double[] array2 = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+    }
+}
