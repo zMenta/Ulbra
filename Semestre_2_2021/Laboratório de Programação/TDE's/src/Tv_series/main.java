a4d9dc45b91eff6bbc0de3cb0b5ec10418cdf932
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-10 22:02:11 -0300

    tv_series

--- Semestre_2_2021/Laboratório de Programação/TDE's/src/Tv_series/main.java
+++ Semestre_2_2021/Laboratório de Programação/TDE's/src/Tv_series/main.java
@@ -1,12 +1,22 @@
 package Tv_series;
 
-import Tv_series.Tv_series;
+
+import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        Tv_series series = new Tv_series("CrabPong");
-        series.info();
-        series.seasons = 5;
-        series.info();
+        /*
+        Tv_series show = new Tv_series("CrabPong");
+        show.info();
+        show.seasons = 5;
+        show.info();
+        */
+
+        int i = 0;
+
+        ArrayList series = new ArrayList();
+        do{
+            int i = 4;
+        }while(i != 5);
     }
 }
