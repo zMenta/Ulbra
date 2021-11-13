35a9fbe60ea40d852a7ba563166c301829a17595
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-13 10:51:08 -0300

    wip

---
+++ Semestre_2_2021/Laboratório de Programação/Java/SemiAS/TvSeries.java
@@ -0,0 +1,44 @@
+package Shows;
+
+import java.util.ArrayList;
+
+public class TvSeries {
+    String title;
+    int seasons;
+    int number_episodes;
+    String summary;
+    Boolean watched;
+
+    public TvSeries(){
+        System.out.println("created a tv series!");
+    }
+
+    // public TvSeries(String title){
+    //     this.title = title;
+    //     System.out.println("created " +this.title+ " tv series");
+    // }
+    
+    public TvSeries(ArrayList list){
+        this.title = list.get(0).toString();
+        this.seasons = Integer.parseInt(list.get(1).toString());
+        System.out.println("Created " +this.title+ " tv series!");
+    }
+
+    // public TvSeries(String title, int seasons, int number_episodes, String summary, Boolean watched){
+    //     this.title = title;
+    //     this.seasons = seasons;
+    //     this.number_episodes = number_episodes;
+    //     this.summary = summary;
+    //     this.watched = watched;
+    //     System.out.println("created " +this.title+ " tv series");
+    // }
+
+    public void info(){
+        System.out.println("Title: "+this.title);
+        System.out.println("Number of Seasons: "+this.seasons);
+        System.out.println("Number of Episodes: "+this.number_episodes);
+        System.out.println("Summary: "+this.summary);
+        System.out.println("Watched: "+this.watched);
+    }
+
+}
--- Semestre_2_2021/Laboratório de Programação/Java/SemiAS/main.java
+++ Semestre_2_2021/Laboratório de Programação/Java/SemiAS/main.java
@@ -1,34 +1,49 @@
-package Tv_series;
+package Shows;
 
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
+
+    static void register(){
+        ArrayList list = new ArrayList();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type the name of the show: ");
+        list.add(input.nextLine());
+        System.out.println("Type the number of seasons: ");
+        list.add(input.nextInt());
+    }
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
         Scanner input = new Scanner(System.in);
         int choice = 0;
 
-        do {
+
+        register();
+
+        do{
             System.out.println("Select a number:");
             System.out.println("1- Add a show. 5 - Exit");
             choice = input.nextInt();
 
-            switch (choice) {
+            switch(choice) {
                 case 1:
                     System.out.println("Type the name of the show: ");
                     list.add(input.nextLine());
                     System.out.println("Type the number of seasons: ");
                     list.add(input.nextInt());
                     break;
-
+                
                 case 5:
                     System.out.println("T");
                     break;
             }
 
-
-        } while (choice != 5);
-
+            
+        }while(choice != 5);
+        
+        TvSeries show = new TvSeries(list);
+        
+        System.out.println("END");
     }
 }
