919422a4a6004963410e95f708fbac38316935eb
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-15 16:12:28 -0300

    wip

--- Semestre_2_2021/Laboratório de Programação/Java/SemiAS/main.java
+++ Semestre_2_2021/Laboratório de Programação/Java/SemiAS/main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class main {
 
+    static void printLine(){
+        System.out.println("----------------------------------------------------------------------");
+    }
+
     public static void main(String[] args) {
         ArrayList<TvSeries> series = new ArrayList();
         ArrayList data = new ArrayList();
@@ -13,35 +17,54 @@ public class main {
         do{
             data.clear();
             System.out.println("Select a number:");
-            System.out.println("1- Add a show | 2- List Shows | 3- Remove a show | 5 - Exit");
+            System.out.println("1- Add a show | 2- List Shows | 3- Remove a show | 4- Total of registered series | 5 - Exit");
             choice = input.nextInt();
 
             switch(choice) {
                 //Add Shows
                 case 1:
+                    printLine();    
                     System.out.println("Type the name of the show: ");
                     data.add(input.next());
                     System.out.println("Type the number of seasons: ");
                     data.add(input.nextInt());
                     TvSeries show = new TvSeries(data);
                     series.add(show);
+                    printLine();    
                     break;
 
                 //List Shows
                 case 2:
+                    printLine();    
                     for(int i = 0; i < series.size(); i++){
                         System.out.println(i + " | "+ series.get(i).title);
                     }
+                    if(series.size() == 0){
+                        System.out.println("There are no registered shows.");
+                    }
+                    printLine();    
                     break;
 
                 //Remove show
                 case 3:
+                    printLine();    
                     System.out.println("Type the show index that you want to remove: ");
                     int index = input.nextInt();
                     series.remove(index);
+                    System.out.println("Removed show of index "+index);
+                    printLine();
+                    break;
 
-                // Exit
-                case 5:
+                //Total of series
+                case 4:
+                    printLine();
+                    System.out.println("Total of registered series: "+series.size());
+                    printLine();
+                    break;
+                    
+                    // Exit
+                    case 5:
+                    printLine();
                     System.out.println("End");
                     break;
             }
@@ -49,6 +72,5 @@ public class main {
             
         }while(choice != 5);
         
-        System.out.println(series.get(0).title);
     }
 }
