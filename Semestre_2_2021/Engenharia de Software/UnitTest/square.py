4c8c64739bdee1c95049ac8d53c610776131f378
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-11-17 08:10:04 -0300

    unittest

--- Semestre_2_2021/Engenharia de Software/UnitTest/square.py
+++ Semestre_2_2021/Engenharia de Software/UnitTest/square.py
@@ -1,4 +1,13 @@
 def square_area(square_side):
+    """
+    Returns the area of a square. 
+    """
+    if type(square_side) not in [int,float]:
+        raise TypeError("Square_side must be an integer or float value.")
+
+    if square_side < 0:
+        raise ValueError("Square_side cannot be negative.")
+    
     area = square_side**2
     return area
     
--- Semestre_2_2021/Engenharia de Software/UnitTest/test_square.py
+++ Semestre_2_2021/Engenharia de Software/UnitTest/test_square.py
@@ -1,6 +1,16 @@
 import unittest
 from square import square_area
 
-class TestCircle(unittest.TestCase):
+class TestSquare(unittest.TestCase):
     def test_area(self):
         self.assertEqual(square_area(5), 25)
+        self.assertEqual(square_area(2), 4)
+        self.assertEqual(square_area(0), 0)
+
+    def test_values(self):
+        self.assertRaises(ValueError, square_area, -4)
+    
+    def test_type(self):
+        self.assertRaises(TypeError, square_area, True)
+        self.assertRaises(TypeError, square_area, 3+5j)
+        self.assertRaises(TypeError, square_area, "five")
