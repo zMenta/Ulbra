0d23a59176fda31fe42aaad5b131696ba8a197ec
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2021-11-28 21:13:18 -0300

    Create PRaticar joins, groups, agregações e having.sql

---
+++ Semestre_2_2021/Fundamentos de Banco de Dados/PRaticar joins, groups, agregações e having.sql
@@ -0,0 +1,24 @@
+--Create tables:
+
+CREATE TABLE Autores(
+  	id serial NOT NULL PRIMARY KEY,
+	nome varchar(40)
+)
+
+CREATE TABLE Editoras(
+	id serial NOT NULL PRIMARY KEY,
+  	nome VARCHAR(40)
+)
+
+CREATE TABLE Livros(
+	id serial NOT NULL PRIMARY KEY,
+  	nome VARCHAR(40),
+  	id_autor INT,
+  	id_editora INT,
+  	CONSTRAINT FK_Autor FOREIGN KEY (id_autor) REFERENCES autores(id),
+  	CONSTRAINT FK_Editora FOREIGN KEY (id_editora) REFERENCES editoras(id)
+)
+
+
+
+--Insert Values:
