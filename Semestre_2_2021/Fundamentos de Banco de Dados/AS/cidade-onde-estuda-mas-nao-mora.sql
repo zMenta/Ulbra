02bb0092dcdd7470487356f992b674ab68742ed0
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-12-03 20:06:59 -0300

    as cidade

--- Semestre_2_2021/Fundamentos de Banco de Dados/AS/cidade-onde-estuda-mas-nao-mora.sql
+++ Semestre_2_2021/Fundamentos de Banco de Dados/AS/cidade-onde-estuda-mas-nao-mora.sql
@@ -2,3 +2,8 @@
 -- Considerando que a cidade onde estudam é Torres.
 -- Exibir o código, o nome do aluno e a cidade onde ele mora
 
+SELECT a.nome as nome_aluno, c.nome as nome_cidade
+FROM aluno a
+INNER JOIN cidade c ON a.idcid = c.idcid
+WHERE c.nome NOT LIKE 'Torres'
+
