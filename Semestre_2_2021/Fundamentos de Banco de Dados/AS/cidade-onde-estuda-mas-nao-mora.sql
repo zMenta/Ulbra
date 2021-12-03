85240cf8b76975e9e04a3e7125ef86e38d03d6ae
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-12-03 19:58:51 -0300

    as

---
+++ Semestre_2_2021/Fundamentos de Banco de Dados/AS/cidade-onde-estuda-mas-nao-mora.sql
@@ -0,0 +1,4 @@
+-- Faça um SQL para selecionar os alunos que não moram na cidade onde estudam.
+-- Considerando que a cidade onde estudam é Torres.
+-- Exibir o código, o nome do aluno e a cidade onde ele mora
+
