7e62943a22b4bf9eda7f43259f880d6bac5b8024
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2021-12-03 20:35:15 -0300

    as

--- Semestre_2_2021/Fundamentos de Banco de Dados/AS/alunos-matriculados.sql
+++ Semestre_2_2021/Fundamentos de Banco de Dados/AS/alunos-matriculados.sql
@@ -2,4 +2,15 @@
 -- Apresentar somente para alunos que tenham se matriculado entre semestre 1 de 2019, considere que as matrículas iniciam em janeiro.
 
 
+ALTER TABLE matricula
+ADD data_matricula DATE;
+
+
+SELECT a.nome as nome_aluno, c.idcurso, c.nomecurso, p.nome as nome_professor, m.data_matricula
+FROM matricula m
+INNER JOIN aluno a ON a.idaluno = m.idaluno
+INNER JOIN turma t ON t.idturma = m.idturma
+INNER JOIN curso c ON c.idcurso = t.idcurso
+INNER JOIN professor p ON p.idprof = t.idprof
+WHERE m.data_matricula BETWEEN '2019-01-01' AND '2019-06-30';
 
