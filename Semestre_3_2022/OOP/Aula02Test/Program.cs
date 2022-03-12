03d160e73db13a9dcdfea7633dd4b37816208b50
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2022-03-11 21:04:49 -0300

    C# aula2

---
+++ Semestre_3_2022/OOP/Aula02Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula02Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
