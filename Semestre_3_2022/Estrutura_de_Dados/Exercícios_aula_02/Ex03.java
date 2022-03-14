843d32a7057a054c4ac56e943e8a0987b0b160c7
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2022-03-14 20:21:57 -0300

    Aula02 | Ex03

---
+++ Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_02/Ex03.java
@@ -0,0 +1,10 @@
+public class Ex03 {
+    public static void main(String[] args) {
+        /*
+         * Leia um conjunto de alunos, cada uma com o nome e a nota. Em seguida exiba o
+         * nome dos alunos que possuem a nota maior do que a média da turma
+         */
+
+        //This exercise is basically the other ones :)
+    }
+}
