62b06343cb2d67bef8da064ef6ec06ee84b9dd11
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2022-03-14 20:54:17 -0300

    Aula02 | Ex04

---
+++ Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_02/Ex04.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        
+        /*Leia um conjunto de valores inteiros e em seguida os exiba-os na ordem inversa do que foram digitados. */
+
+        // Already "done" in aula01 exercises.
+
+    }
+    
+}
