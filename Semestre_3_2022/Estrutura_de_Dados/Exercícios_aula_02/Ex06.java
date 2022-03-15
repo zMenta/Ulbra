4ba690ae2b380c231115a43338d132a8b15a68a7
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2022-03-14 21:01:16 -0300

    Aula02 | Ex06

---
+++ Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_02/Ex06.java
@@ -0,0 +1,9 @@
+public class Ex06 {
+    public static void main(String[] args) {
+        /*
+         * Crie um vetor capaz de armazenar 50 números inteiros. Em seguida faça o seu
+         * preenchimento automático de forma randômica. Em seguida, exiba os valores
+         * deste vetor.
+         */
+    }
+}
