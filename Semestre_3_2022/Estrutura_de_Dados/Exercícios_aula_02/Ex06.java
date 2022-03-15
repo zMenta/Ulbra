9ba1ba54ee72d93f35b3230e4457eb1e3b38db7a
brentano.l@gmail.com <70714721+zMenta@users.noreply.github.com>
2022-03-14 21:01:24 -0300

    Aula02 | Ex06

--- Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_02/Ex06.java
+++ Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_02/Ex06.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Ex06 {
     public static void main(String[] args) {
         /*
@@ -5,5 +7,16 @@ public class Ex06 {
          * preenchimento automático de forma randômica. Em seguida, exiba os valores
          * deste vetor.
          */
+
+        Random random = new Random();
+        int[] array = new int[50];
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
