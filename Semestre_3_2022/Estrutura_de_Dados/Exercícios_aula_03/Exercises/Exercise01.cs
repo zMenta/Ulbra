4ceebd26db0117e0a271b0702d20dfee52307cbb
LucasBrentano <70714721+zMenta@users.noreply.github.com>
2022-03-22 20:03:59 -0300

    wip

---
+++ Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_03/Exercises/Exercise01.cs
@@ -0,0 +1,15 @@
+namespace Exercícios_aula_03.Exercises
+{
+    public class Exercise01
+    {
+        public static void run()
+        {
+            System.Console.WriteLine("Exercise 01");
+        }
+
+        // public Exercise01(){
+        //     System.Console.WriteLine("Exercise 01");
+        // }
+
+    }
+}
---
+++ Semestre_3_2022/Estrutura_de_Dados/Exercícios_aula_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Exercícios_aula_03.Exercises;
+
+namespace Exercícios_aula_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var exerciseMap = new Dictionary<string, Action>
+            {
+                {"1", Exercise01.run}
+            };
+
+
+            // Exercise01.run();
+
+            // Exercise01 ex = new Exercise01();
+
+            if( true){
+                exerciseMap.TryGetValue(args[0], out var execiseAction);
+                execiseAction();
+            }
+
+        }
+    }
+}
