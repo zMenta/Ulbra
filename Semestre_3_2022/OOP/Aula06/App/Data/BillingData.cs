c4816f033be403c78ecaf38f76e1e7e0bb01b2a0
Lucas Brentano <lucas.brentano@ubistart.com>
2022-04-11 11:04:11 -0300

    OOP | Aula06 | BillingData Update

--- Semestre_3_2022/OOP/Aula06/App/Data/BillingData.cs
+++ Semestre_3_2022/OOP/Aula06/App/Data/BillingData.cs
@@ -1,29 +1,36 @@
+using System.Collections.Generic;
+using Aula06.App.Model;
+
 namespace Aula06.App.Data
 {
     public class BillingData
     {
-        private List<Billing> bilingList= new List<Billing>();
+        private List<Billing> billingList = new List<Billing>();
 
-        public void GetAll()
+        public List<Billing> GetAll()
         {
-            return bilingList;
+            return billingList;
         }
 
-        public void GetById(int id){
-            return bilingList.Find(bill => bill.Id == id);
+        public Billing GetById(int id)
+        {
+            return billingList.Find(bill => bill.Id == id);
         }
 
-        public void Add(Billing billing){
+        public void Add(Billing billing)
+        {
             billingList.Add(billing);
         }
 
-        public void UpdateById(int id, Billing newBilling){
+        public void UpdateById(int id, Billing newBilling)
+        {
             var oldBilling = GetById(id);
 
             oldBilling = newBilling;
         }
 
-        public void Delete(Billing billing){
+        public void Delete(Billing billing)
+        {
             billingList.Remove(billing);
         }
 
