c135c19f18008e89a1e32463b6eb377b5f94818b
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-05-31 16:34:29 -0300

    EDD | Recursive 2 better naming

--- Semestre_3_2022/Estrutura_de_Dados/Atividade_recursividade_18/2.py
+++ Semestre_3_2022/Estrutura_de_Dados/Atividade_recursividade_18/2.py
@@ -6,11 +6,11 @@ def max_value(array, i=len(array)):
     if i <= 1:
         return array[0]
 
-    current = max_value(array, i-1)
-    if array[i-1] > current:
+    highest_value = max_value(array, i-1)
+    if array[i-1] > highest_value:
         return array[i-1]
     else:
-        return current
+        return highest_value
 
 
 print(max_value(array))
