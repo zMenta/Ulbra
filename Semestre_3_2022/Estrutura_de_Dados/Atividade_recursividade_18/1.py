5edd552aaa64c192622bc1eec1ad805286af91f7
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-05-31 15:52:03 -0300

    EDD | Recursive 1

---
+++ Semestre_3_2022/Estrutura_de_Dados/Atividade_recursividade_18/1.py
@@ -0,0 +1,11 @@
+
+array = [2, 8, 10, 5, 25]
+
+# Should return 50 from the example array.
+def sum_array(array, i=len(array)):
+    if i <= 0:
+        return 0
+    return sum_array(array, i-1) + array[i-1]
+
+
+print(sum_array(array))
