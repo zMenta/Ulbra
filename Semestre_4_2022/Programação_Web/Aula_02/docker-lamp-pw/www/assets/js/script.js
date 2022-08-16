731d2ae5f9b22e0c003145df17097f3303ecac9a
Lucas Brentano <brentano.l@gmail.com>
2022-08-16 17:02:50 -0300

    Web | Basic Button presses

--- Semestre_4_2022/Programação_Web/Aula_02/docker-lamp-pw/www/assets/js/script.js
+++ Semestre_4_2022/Programação_Web/Aula_02/docker-lamp-pw/www/assets/js/script.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
 
     $('section').load('Pages/home.html');
 
+    $('#buttonAbout').click(function () {
+        $('section').load('Pages/about.html');
+    });
+
+    $('#buttonHome').click(function () {
+        $('section').load('Pages/home.html');
+    });
 });
