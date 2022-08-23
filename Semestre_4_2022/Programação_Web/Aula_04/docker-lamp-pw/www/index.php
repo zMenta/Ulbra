b53e8166ef0c639403e95c687ea077e0436cf4ae
Lucas Brentano <brentano.l@gmail.com>
2022-08-23 19:19:06 -0300

    Web | Aula_04 Start

---
+++ Semestre_4_2022/Programação_Web/Aula_04/docker-lamp-pw/www/index.php
@@ -0,0 +1,45 @@
+<?php
+
+    if (!isset($_GET['controller'])) {
+        require_once('Controllers/SiteController.php');
+        $SiteController = new SiteController();
+        $SiteController -> home();
+    }else {
+        
+        switch ($_REQUEST['controller']) {
+            case 'site':
+                require_once('Controllers/SiteController.php');
+                $SiteController = new SiteController();
+                
+                if (!isset($_GET['method'])) {
+                    $SiteController -> home();
+                }else {
+                    switch ($_REQUEST['method']) {
+                        case 'home':
+                            $SiteController -> home();
+                            break;
+
+                        case 'about':
+                            $SiteController -> about();
+                            break;
+                        
+                        case 'products':
+                            $SiteController -> products();
+                            break;
+                        
+                        case 'contact':
+                            $SiteController -> contact();
+                            break;
+                    }
+                }
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+
+    }
+
+
+?>
