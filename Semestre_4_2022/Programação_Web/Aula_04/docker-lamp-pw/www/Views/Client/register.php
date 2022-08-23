6a52fbfc4ba7bf430e5cd88092b9d32b779bd904
Lucas Brentano <brentano.l@gmail.com>
2022-08-23 20:58:55 -0300

    Web | ClienRegister - Radial Buttons

--- Semestre_4_2022/Programação_Web/Aula_04/docker-lamp-pw/www/Views/Client/register.php
+++ Semestre_4_2022/Programação_Web/Aula_04/docker-lamp-pw/www/Views/Client/register.php
@@ -8,6 +8,13 @@
         <label class='form-label' for="">Email</label>
         <input class='form-control' type="email" name="email">
     </div>
+    <div class='mb-3 mt-3'>
+        <h4>Gender</h4>
+        <input type="radio" name="fav_language" value="HTML">
+        <label class="form-label">Female</label><br>
+        <input type="radio" name="fav_language" value="CSS">
+        <label class="form-label">Male</label>
+    </div>
     
     <button class='btn btn-primary' type='submit'>Send</button>
 </form>
