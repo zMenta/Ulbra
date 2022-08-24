401e14b029bcab69d730490a003991faf38b533c
Lucas Brentano <brentano.l@gmail.com>
2022-08-23 21:27:03 -0300

    Web | TDE - ClientRegister Checkbox

--- Semestre_4_2022/Programação_Web/Aula_04/docker-lamp-pw/www/Views/Client/register.php
+++ Semestre_4_2022/Programação_Web/Aula_04/docker-lamp-pw/www/Views/Client/register.php
@@ -4,15 +4,26 @@
         <label class='form-label' for="">Name</label>
         <input class='form-control' type="text" name="name">
     </div>
+    
     <div class='mb-3 mt-3'>
         <label class='form-label' for="">Email</label>
         <input class='form-control' type="email" name="email">
     </div>
+    
     <div class='mb-3 mt-3'>
         <h4>Gender</h4>
         <label><input type="radio" name="gender" value="female" checked> Female</label>
         <br>
         <label><input type="radio" name="gender" value="male" checked> Male</label>
+    </div>
+
+    <div class="mb-3 mt-3">
+        <h4>Terms and use</h4>
+        <label><input type="checkbox" name="client_term" checked > I have read and accept the client registration terms. </label>
+        <br>
+        <label><input type="checkbox" name="client_spam" checked > I want to receive spam and special offers in my email. </label>
+        <br>
+        <label><input type="checkbox" name="client_notification" checked > I want to keep up with the service updates and news. </label>
     </div>
     
     <button class='btn btn-primary' type='submit'>Send</button>
