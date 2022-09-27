68d1ebfeec7a48c2bf54dd70fce0cf7ecb2dffc9
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-09-27 19:04:27 -0300

    Web | Aula10

---
+++ Semestre_4_2022/Programação_Web/Aula_10/docker-lamp-pw/www/Admin/index.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+if (!isset($_GET['controller'])) {
+    require_once('controllers/MainController.php');
+    $MainController = new MainController();
+    $MainController->home();
+} else {
+
+    switch ($_REQUEST['controller']) {
+        case 'main':
+
+            require_once('controllers/MainController.php');
+            $MainController = new MainController();
+
+            if (!isset($_GET['method'])) {
+                $MainController->home();
+            } else {
+                switch ($_REQUEST['method']) {
+                    case 'home':
+                        $MainController->home();
+                        break;
+
+                    case 'login':
+                        $MainController->login();
+                        break;
+
+                    case 'logout':
+                        $MainController->logout();
+                        break;
+                }
+            }
+            break;
+
+        case 'user':
+            require_once('controllers/UserController.php');
+            $UserController = new UserController();
+
+            switch ($_REQUEST['method']) {
+                case 'validateLogin':
+                    $UserController->validateLogin();
+                    break;
+            }
+
+            break;
+    }
+}
---
+++ Semestre_4_2022/Programação_Web/Aula_10/docker-lamp-pw/www/Views/Client/registerView.php
@@ -0,0 +1,39 @@
+<h1>Registration Complete</h1>
+<table>
+    <tr>
+        <th>Name: </th>
+        <td>
+            <?= $client_data_array['name']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Email: </th>
+        <td>
+            <?= $client_data_array['email']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Gender: </th>
+        <td>
+            <?= $client_data_array['gender']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Terms: </th>
+        <td>
+            <?= $client_data_array['term']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Spam: </th>
+        <td>
+            <?= $client_data_array['spam']?>
+        </td>
+    </tr>
+    <tr>
+        <th>Notifications: </th>
+        <td>
+            <?= $client_data_array['notification']?>
+        </td>
+    </tr>
+</table>
