dbbd6ad908bca7a5a0f8f68234a732be60ac4443
LucasBrentano <brentano.l@gmail.com>
2022-10-26 11:47:45 -0300

    web | contactController

--- Semestre_4_2022/Programação_Web/Aula_12_API/docker-lamp-pw/www/api/Controllers/ContactController.php
+++ Semestre_4_2022/Programação_Web/Aula_12_API/docker-lamp-pw/www/api/Controllers/ContactController.php
@@ -9,6 +9,31 @@ class ContactControllor{
     }
 
 
+    public function listContacts(){
+       $result = $this -> ContactModel -> listContacts();
+
+       $contactsArray = array();
+
+        while($contact = $result -> fetch_assoc()){
+           array_push($contactsArray, $contact);
+       }
+        header('Contenty-Type: application/json');
+        echo json_encode($contactsArray);
+    }
+
+
+    public function consultContact($contactId){
+        $result = $this -> ContactModel -> consultContact($contactId);
+
+        if($contactArray = $result -> fetch_assoc()){
+            header('Contenty-Type: application/json');
+            echo json_encode($contactArray);
+        }else{
+            header('Contenty-Type: application/json');
+            echo('{ "error" : "Contact not found" }');
+        }
+    }
+
 
 }
 ?>
