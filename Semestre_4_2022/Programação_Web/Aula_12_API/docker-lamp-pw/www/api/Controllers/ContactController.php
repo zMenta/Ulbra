fc7aa0483a119baecf82a857c253ab452c553afb
LucasBrentano <brentano.l@gmail.com>
2022-10-26 11:59:42 -0300

    web | contactController typo fix

--- Semestre_4_2022/Programação_Web/Aula_12_API/docker-lamp-pw/www/api/Controllers/ContactController.php
+++ Semestre_4_2022/Programação_Web/Aula_12_API/docker-lamp-pw/www/api/Controllers/ContactController.php
@@ -1,10 +1,10 @@
 <?php
-class ContactControllor{
+class ContactController{
 
     var $ContactModel;
 
     public function __construct(){
-        require_once('Models/ContactModel.php')
+        require_once('Models/ContactModel.php');
         $this -> ContactModel = new ContactModel();
     }
 
