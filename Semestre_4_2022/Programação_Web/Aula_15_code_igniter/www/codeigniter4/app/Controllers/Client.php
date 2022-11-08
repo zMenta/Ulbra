0464bac5c7c2463e98831e7d7300edf4642dbff8
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-11-08 20:19:44 -0300

    client controller

---
+++ Semestre_4_2022/Programação_Web/Aula_15_code_igniter/www/codeigniter4/app/Controllers/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Controllers\BaseController;
+
+class CLient extends BaseController{
+
+    public function listClients(){
+        // $ClientModel = new ClientModel();
+    
+        // $data = [
+        //     'arrayClients' => $ClientModel -> findAll()
+        // ];
+    
+        // echo view ('templates/header');
+        // echo view ('templates/listClients', $data);
+        // echo view ('templates/footer');
+    }
+
+}
