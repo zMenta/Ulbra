b727ef0145f74590cc94cfcf22b85a3c8308c561
menta <brentano.l@gmail.com>
2023-10-12 13:23:36 -0300

    components ap2 | basic footer style and component

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/App.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
   return (
+	<>
     <div className="App">
-	  <Header titleLeft={"Ulbra"} title={"Entrevero"}></Header>
+		<Header titleLeft={"Ulbra"} title={"Entrevero"}></Header>
+		<Footer></Footer>
     </div>
+	</>
   );
 }
 
---
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../styles/Footer.css"
+
+function Footer() {
+	return(
+		<footer className="Footer">
+			<h5>AP2 - Lucas Vianna Moog Brentano</h5>
+		</footer>
+	)
+}
+
+export default Footer;
