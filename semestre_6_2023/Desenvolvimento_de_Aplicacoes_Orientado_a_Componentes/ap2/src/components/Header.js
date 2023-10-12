5158f05d1e75e1da47a0f22931f66bbb47da8fc5
menta <brentano.l@gmail.com>
2023-10-12 12:27:41 -0300

    components ap2 | Basic header component and style

---
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/Header.js
@@ -0,0 +1,13 @@
+import "../styles/Header.css";
+
+function Header({ title }) {
+	return (
+		<>
+		<header class="Header">
+		<h1> {title} </h1>
+		</header>
+		</>
+	)
+}
+
+export default Header;
