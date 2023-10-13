3d673c045e307d9b861d45c0a6b79fd5eb4b1e1c
menta <brentano.l@gmail.com>
2023-10-12 21:11:04 -0300

    components ap2 | created HyperContent class, updated footer

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/Footer.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/Footer.js
@@ -1,9 +1,15 @@
 import "../styles/Footer.css"
+import HyperContent from "./HyperContent";
 
 function Footer({ title }) {
 	return(
 		<footer className="Footer">
 			<h5>{title}</h5>
+			<HyperContent
+				title={"github"}
+				url={"https://github.com/zMenta/Ulbra/tree/main/semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2"}
+				imageUrl={"https://www.pngall.com/wp-content/uploads/2016/04/Github-PNG-Image.png"}
+			></HyperContent>
 		</footer>
 	)
 }
---
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/HyperContent.js
@@ -0,0 +1,34 @@
+import "../styles/HyperContent.css"
+
+function HyperContent({ title, url, imageUrl, imageSize}) {
+	if(!imageSize){
+		imageSize = 45
+	}
+
+	if(!imageUrl){
+		return(
+			<div className="HyperContent">
+				<a href={url} target="_blank" rel="noreferrer">{title}</a>
+			</div>
+		)
+	}
+
+	else{
+		return (
+			<div className="HyperContent">
+				<a href={url} target="_blank" rel="noreferrer">
+					<img
+						src={imageUrl}
+						alt={title}
+						width={imageSize}
+						height={imageSize}
+					>
+					</img>
+				</a>
+				<a href={url} target="_blank" rel="noreferrer">{title}</a>
+			</div>
+			)
+	}
+}
+
+export default HyperContent;
