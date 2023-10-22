5f7b5c18e90515bc42a99df66e2845e182c715c0
moog <brentano.l@gmail.com>
2023-10-22 14:27:36 -0300

    components ap2 | CepContent missing props and button function

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/Body.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/Body.js
@@ -8,13 +8,18 @@ import RegisterPerson from "./RegisterPerson";
 
 function Body() {
 	const [personsArray, setPersonsArray] = useState(persons)
+	const [cepVisible, setCepVisible] = useState(true)
+
+	function switchCepVisible(){
+		setCepVisible(!cepVisible)
+	}
 
 	return(
 		<>
 			<Container alignStart={true}>
 				<Container column={true}>
-					<Button title={"CEP Button"}/>
-					<CepContent/>
+					<Button title={"CEP Button"} callback={switchCepVisible}/>
+					<CepContent isVisible={cepVisible} streetName={"A street name"} dateTime={"15:40"}/>
 				</Container>
 				<RegisterPerson personsArray={personsArray} setPersons={setPersonsArray}/>
 				<CardContainer personsArray={personsArray}/>
--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/CepContent.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/ap2/src/components/CepContent.js
@@ -1,16 +1,22 @@
 import "../styles/CepContent.css"
 import useFetch from "../hooks/useFetch";
 
-function CepContent(){
+function CepContent({ isVisible = true, streetName="", dateTime=""}){
 	const [data] = useFetch("https://viacep.com.br/ws/95560000/json/")
 	console.log(data)
+
+	if(!isVisible){
+		return
+	}
 
 	return(
 		<div className="CepContent">
 			<h3>CEP | {data.cep}</h3>
-			<p>Cidade: {data.localidade} - {data.uf}</p>
-			<p>Bairro: {data.bairro}</p>
-			<p>Complemento: {data.complemento}</p>
+			<p>City: {data.localidade} - {data.uf}</p>
+			<p>Neighborhood: {data.bairro}</p>
+			<p>Complement: {data.complemento}</p>
+			<p>Street: {streetName}</p>
+			<p>Time: {dateTime}</p>
 		</div>
 	)
 }
