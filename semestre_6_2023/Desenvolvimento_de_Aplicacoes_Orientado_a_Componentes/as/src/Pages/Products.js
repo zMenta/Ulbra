65259ebb551c5ac1beab928d89b584e0721f217b
moog <brentano.l@gmail.com>
2023-11-22 20:13:58 -0300

    Components AS | Fetching products name from db.json

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/Pages/Products.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/Pages/Products.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import Header from "../Components/Header";
 
 function Products(){
+	// TODO Future custom hook?
+	const [products, setProducts] = useState([])
+	useEffect(() => {
+		async function fetchProducts() {
+			const result = await fetch("http://localhost:3001/products")
+			setProducts(await result.json())
+		}
+
+		fetchProducts()
+	}, [])
+
 	return(
 		<>
 			<Header title={"Products page"}/>
+			{products && products.map((product) =>{
+				return <h1>{product.name}</h1>
+			})}
 		</>
 	)
 }
