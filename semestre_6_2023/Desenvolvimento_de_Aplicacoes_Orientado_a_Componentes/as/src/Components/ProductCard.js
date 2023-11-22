f46aadf6f46195f9f1131e3223be0d52aa84f5aa
moog <brentano.l@gmail.com>
2023-11-22 20:28:40 -0300

    Components AS | ProductCard style update

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/Components/ProductCard.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/Components/ProductCard.js
@@ -1,6 +1,6 @@
 function ProductCard({product}) {
 	return(
-		<div style={{ backgroundColor: "lightgrey", padding:8, margin:10, width: "20%" }}>
+		<div style={{ backgroundColor: "lightgrey", padding:8, margin:10, width: 450 }}>
 			<h2>{product.name}</h2>
 			<p>Price: ${product.price} </p>
 			<p>Description: {product.description}</p>
