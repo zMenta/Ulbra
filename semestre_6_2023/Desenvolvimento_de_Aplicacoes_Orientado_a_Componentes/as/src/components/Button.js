b2e1d041efae161bca7df13e6ec092f91ef14864
moog <brentano.l@gmail.com>
2023-11-24 10:34:20 -0300

    Components AS | Fixed button component typo

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/components/Button.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/components/Button.js
@@ -1,6 +1,6 @@
 function Button({ title, callback}) {
 	return(
-		<button class="Button" onClick={callback} type="button">{title}</button>
+		<button className="Button" onClick={callback} type="button">{title}</button>
 	)
 }
 
