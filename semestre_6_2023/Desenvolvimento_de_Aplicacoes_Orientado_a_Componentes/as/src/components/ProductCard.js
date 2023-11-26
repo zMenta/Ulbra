a0ddc1221b3c843cba33f3a587ab06194c058267
moog <brentano.l@gmail.com>
2023-11-26 19:35:27 -0300

    Components AS | Updated home page, Products fetch image_url

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/components/ProductCard.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/components/ProductCard.js
@@ -9,7 +9,7 @@ function ProductCard({product}) {
 
 	return(
 		<div onClick={() => focusProduct(product.id)} className='flex-col flex justify-start bg-gray-900 border-zinc-100 m-2 h-96 w-96 shadow-black rounded-lg hover:bg-gray-700 hover:scale-105' >
-			<img className="rounded h-72 " src="https://media.gettyimages.com/id/185318363/photo/office-armchair-clipping-path.jpg?s=612x612&w=0&k=20&c=cqlTWgGE1msfJmsA8hYFUYGvS3AkWUCd08Vuw3Pudjs=" alt={product.name}/>
+			<img className="rounded h-72 " src={product.photo_url} alt={product.name}/>
 			<div className="m-2">
 			<h2 className="font-bold text-lg mb-2">{product.name}</h2>
 			<p>Price: ${product.price} </p>
--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/pages/Home.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/pages/Home.js
@@ -4,6 +4,11 @@ function Home() {
 	return (
 	<>
 		<Header title={"Cool Service"}/>
+		<div>
+			<h1 className="m-20 text-6xl">Really cool Home page!</h1>
+			<h2 className="ml-20 text-2xl">Component Oriented Programming - AS</h2>
+			<h3 className="ml-20 text-xl">Lucas Vianna Moog Brentano</h3>
+		</div>
 	</>
 	)
 }
