b24dd43c6e91f9c7c1954f71387b594c807a989d
moog <brentano.l@gmail.com>
2023-11-26 19:43:03 -0300

    Components AS | Product not found style

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/pages/ProductNotFound.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/pages/ProductNotFound.js
@@ -1,15 +1,14 @@
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 function ProductNotFound(){
-	const params = useParams()
-
 	return(
 		<>
 		<Header title={"Product not found!"}/>
-		<h1>:(</h1>
-		<p>Product not found of id: {params.id}</p>
-		<Link to="/products">Go back</Link>
+		<div className="items-center justify-center bg-red-800 rounded-lg flex flex-col m-10 mt-8 p-4 h-[75vh] w-[54vw] shadow-black">
+			<h1 className="font-bold text-3xl mb-10">Product not found! :(</h1>
+			<Link className="bg-blue-900 rounded-lg hover:bg-blue-700 mt-20 w-20 text-center" to="/products">  Go back  </Link>
+		</div>
 		</>
 	)
 }
