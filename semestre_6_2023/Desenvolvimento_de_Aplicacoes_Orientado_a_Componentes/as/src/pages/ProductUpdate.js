3a39eaea08303742c7c93dbee00bf9b9fa121ebc
moog <brentano.l@gmail.com>
2023-11-26 18:22:06 -0300

    Components AS | Update product style

--- semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/pages/ProductUpdate.js
+++ semestre_6_2023/Desenvolvimento_de_Aplicacoes_Orientado_a_Componentes/as/src/pages/ProductUpdate.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import AboutUs from "../components/AboutUs";
 import Button from "../components/Button";
+import ErrorMessage from "../components/ErrorMessage";
 import Header from "../components/Header";
+import SucessMessage from "../components/SucessMessage";
 
 function ProductUpdate(){
 	const [id, setId] = useState(-1)
@@ -38,42 +41,53 @@ function ProductUpdate(){
 		setResponse(resp)
 	}
 
+	// {!isValid && <h4>All values must be filled and price can't be 0! Id must be higher than -1</h4>}
+
 	return(
 		<>
 			<Header title={"Update a product"}/>
-			{!isValid && <h4>All values must be filled and price can't be 0! Id must be higher than -1</h4>}
-			{response.status === 200 && isValid && <h3>Product updated</h3>}
-			{response.status !== 200 && response.status && <h3>Error: {response.status} - {response.statusText}</h3>}
-			<form>
-				<label> Product ID: 
-					<input type="number" value={id} onChange={(event) => {
-						setId(event.target.value)
-					}} />
-				</label>
-				<label> Name: 
-					<input type="text" value={name} onChange={(event) => {
-						setName(event.target.value)
-					}} />
-				</label>
-				<label> Price: 
-					<input type="number" value={price} onChange={(event) => {
-						setPrice(event.target.value)
-					}} />
-				</label>
-				<label> Description: 
-					<input type="text" value={description} onChange={(event) => {
-						setDescription(event.target.value)
-					}} />
-				</label>
-				<label> Image URL: 
-					<input type="text" value={imageUrl} onChange={(event) => {
-						setImageUrl(event.target.value)
-					}} />
-				</label>
-			</form>
-			<br/>
-			<br/>
-			<Button title={"Update"} callback={updateProduct}/>
+			<div className="flex flex-row">
+				<AboutUs/>
+				<div className="items-center bg-gray-900 rounded-lg flex flex-col justify-start m-10 mt-8 p-4 h-[65vh] w-[24vw] shadow-black">
+					<form>
+						<h2 className="mb-8 font-bold text-lg" >Update a product</h2>
+						<div>
+							<p>Id:</p>
+							<input className="bg-gray-600 rounded" type="number" value={id} onChange={(event) => {
+								setId(event.target.value)
+							}} />
+						</div>
+						<div>
+							<p>Name:</p>
+							<input className="bg-gray-600 rounded" type="text" value={name} onChange={(event) => {
+								setName(event.target.value)
+							}} />
+						</div>
+						<div>
+							<p className="mt-4" >Price:</p>
+							<input className="bg-gray-600 rounded" type="number" value={price} onChange={(event) => {
+								setPrice(event.target.value)
+							}} />
+						</div>
+						<div>
+							<p className="mt-4" >Description:</p>
+							<input className="bg-gray-600 rounded" type="text" value={description} onChange={(event) => {
+								setDescription(event.target.value)
+							}} />
+						</div>
+						<div>
+							<p className="mt-4" >Description:</p>
+							<input className="bg-gray-600 rounded" type="text" value={imageUrl} onChange={(event) => {
+								setImageUrl(event.target.value)
+							}} />
+						</div>
+					</form>
+					<Button title={"Update"} callback={updateProduct}/>
+					{!isValid && <ErrorMessage text="All values must be filled and price can't be 0. Id must be higher than -1"/>}
+					{response.status === 200 && isValid && <SucessMessage text="Product updated!"/> }
+					{response.status !== 200 && response.status && <ErrorMessage text={`Error: ${response.status} - ${response.statusText}`}/>}
+				</div>
+			</div>
 		</>
 	)
 }
